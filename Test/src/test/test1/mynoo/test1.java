600495088afbef140d6998e27d0bcf8b8dc6e643
Jo MinWoo <Jo MinWoo@DESKTOP-ACBG020>
2021-05-08 14:43:40 +0900

    second

--- Test/src/test/test1/mynoo/test1.java
+++ Test/src/test/test1/mynoo/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		System.out.println("여기는 테스트 파일입니다.");
-		System.out.println("테스트 2번째l");
+		System.out.println("테스트 2번째2");
 		
 		
 	}
