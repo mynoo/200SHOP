536a1506bf561fada2f87b0fe2c6f9f69a5cbb10
Jo MinWoo <Jo MinWoo@DESKTOP-ACBG020>
2021-05-16 15:09:05 +0900

    충돌 해결중

--- T.A.S shop/src/main/java/shopping/mall/controller/MallListController.java
+++ T.A.S shop/src/main/java/shopping/mall/controller/MallListController.java
@@ -1,6 +1,8 @@
 package shopping.mall.controller;
 
 import java.io.IOException;
+
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import shopping.common.controller.SuperClass;
 import shopping.common.model.ShoppingInfo;
+import shopping.mall.model.MyCart;
 import shopping.member.controller.MemberLoginController;
 import shopping.product.controller.ProductListController;
 import shopping.product.model.Product;
@@ -24,6 +27,56 @@ public class MallListController extends SuperClass {
 		
 		if (super.session.getAttribute("loginfo") == null) {
 			new MemberLoginController().doGet(request, response);
+			
+		} else {
+			MyCart mycart = (MyCart)super.session.getAttribute("mycart") ;
+			if (mycart == null) {
+				String message = "쇼핑 목록이 없어서 상품 목록 페이지로 이동합니다." ;
+				super.setErrorMessage(message);
+				
+				new ProductListController().doGet(request, response);
+			} else if(mycart != null){
+				Map<Integer, Integer> maplists = mycart.GetAllOrderList() ;
+				System.out.println("cart item size : " + maplists.size());
+				
+				Set<Integer> keylist = maplists.keySet() ;
+				
+				List<ShoppingInfo> shoplists = new ArrayList<ShoppingInfo>();
+				
+				int totalAmount = 0 ; //총 금액
+				int totalPoint = 0 ; //총 누적 포인트
+				
+				for(Integer pnum : keylist) {
+					// pnum는 상품 번호
+					
+					int qty = maplists.get(pnum) ;
+					
+					ProductDao dao = new ProductDao();
+					Product bean = dao.SelectDataByPk(pnum) ;
+					
+					// shopInfo : 상품 1개에 대한 정보를 저장하기 위한 클래스
+					ShoppingInfo shopInfo = new ShoppingInfo() ;
+					
+					int price = bean.getPrice() ;
+					
+					totalAmount += qty * price ;
+					
+					shopInfo.setImage(bean.getImage());
+					shopInfo.setPname(bean.getPname());
+					shopInfo.setPnum(pnum);
+					shopInfo.setPrice(price);
+					shopInfo.setQty(qty);
+					
+					shoplists.add(shopInfo) ;
+				}
+				
+				super.session.setAttribute("totalAmount", totalAmount);
+				super.session.setAttribute("totalPoint", totalPoint);
+				super.session.setAttribute("shoplists", shoplists);
+				
+				String gotopage = "/mall/mallList.jsp" ; 
+				super.GotoPage(gotopage);
+			}
 		}
 	}
 }
